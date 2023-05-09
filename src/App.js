6833e19a4557386da3865cb8409b788187dd7dba
mannybennett <mannybennett99@gmail.com>
2023-05-08 21:46:40 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      isClicked: false,
+      todos: [],
+      text: ''
+    }
+  }
+
+  onClickHandler = () => {
+    this.state.isClicked ? this.setState({isClicked : false}) : this.setState({isClicked : true})
+  }
+
+  onChangeHandler = e => this.setState({text: e.target.value})
+
+  onSubmitHandler = e => {
+    this.setState({todos: [...this.state.todos, this.state.text]})
+    this.setState({text: ""})
+  }
+
+  // onDeleteHandler = () => {
+  //   this.state.todos.filter(i => {
+
+  //   })
+  // }
+
+  render() {
+    return (
+      <div style={{ marginTop: '400px' }} className="App">
+        <button type="button" onClick={this.onClickHandler}>
+          {this.state.isClicked ? 'Yes' : 'No'}
+        </button>
+        <input type='text' onChange={this.onChangeHandler}></input>
+        <button type='submit' onClick={this.onSubmitHandler}>Submit</button>
+        <ol>
+          {this.state.todos.map((todo, idx) => {
+            return <li key={idx}>
+              {todo}
+              <button type='button'>Delete</button>
+              </li>
+          })}
+        </ol>
+      </div>
+    );
+  }
 }
 
 export default App;
